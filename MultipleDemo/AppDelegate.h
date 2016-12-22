3e537b9d462e514b6b18b828433fab6ddd016a49
Lijinz <lijinb.kunju@gmail.com>
2016-12-22 15:27:15 +0530

    Initial Commit

---
+++ MultipleDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultipleDemo
+//
+//  Created by Cybraum on 12/22/16.
+//  Copyright © 2016 Cybraum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultipleDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MultipleDemo
+//
+//  Created by Cybraum on 12/22/16.
+//  Copyright © 2016 Cybraum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
